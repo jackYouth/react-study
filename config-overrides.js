a2e649eafaab77d7a214b4a4d0d43152dc397806
jackyouth <1849552082@qq.com>
2019-12-04 11:32:18 +0800

    first init

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const {
+  override,
+  fixBabelImports,
+  addWebpackPlugin
+} = require("customize-cra");
+
+const AntdDayjsWebpackPlugin = require("antd-dayjs-webpack-plugin");
+
+module.exports = override(
+  // antd的默认样式改为按需加载
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: "css"
+  }),
+  // 使用day.js替换moment.js, 减少包体积
+  addWebpackPlugin(new AntdDayjsWebpackPlugin())
+);
