175ef2c322f8e56886463337282644fa6bbf44a0
suyd <yandong.su@ifchange.com>
2020-08-27 15:43:59 +0800

    feat: commit lint & auto prettier

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './index.module.scss';
 
 export default function Dashboard() {
+  console.log(test);
   return (
     <div className={styles.dashboard}>
       <h2>Dashboard</h2>
