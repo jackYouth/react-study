74bbf564f8bee3b698a61d5511883521d257dea5
suyd <yandong.su@ifchange.com>
2020-08-27 16:05:13 +0800

    feat: commitlint

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,13 @@
-const {
-  override,
-  fixBabelImports,
-  addWebpackPlugin,
-  addWebpackAlias,
-  overrideDevServer,
-} = require("customize-cra");
-const path = require("path");
+const {override, fixBabelImports, addWebpackPlugin, addWebpackAlias, overrideDevServer} = require('customize-cra');
+const path = require('path');
 
-const AntdDayjsWebpackPlugin = require("antd-dayjs-webpack-plugin");
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
 
 const addProxy = () => (config) => {
   config.proxy = {
-    "/api": {
-      target: "http://yapi.ifchange.com",
-      pathRewrite: { "/api": "/mock/1096/api" },
+    '/api': {
+      target: 'http://yapi.ifchange.com',
+      pathRewrite: {'/api': '/mock/1096/api'},
       changeOrigin: true,
     },
   };
@@ -23,20 +17,20 @@ const addProxy = () => (config) => {
 module.exports = {
   webpack: override(
     // antd的默认样式改为按需加载
-    fixBabelImports("import", {
-      libraryName: "antd",
-      libraryDirectory: "es",
-      style: "css",
+    fixBabelImports('import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: 'css',
     }),
     // 使用day.js替换moment.js, 减少包体积
     addWebpackPlugin(new AntdDayjsWebpackPlugin()),
     // 添加别名
     addWebpackAlias({
-      "@components": path.join(__dirname, "./src/components"),
-      "@pages": path.join(__dirname, "./src/pages"),
-      "@utils": path.join(__dirname, "./src/utils"),
-      "@config": path.join(__dirname, "./src/config"),
-    })
+      '@components': path.join(__dirname, './src/components'),
+      '@pages': path.join(__dirname, './src/pages'),
+      '@utils': path.join(__dirname, './src/utils'),
+      '@config': path.join(__dirname, './src/config'),
+    }),
   ),
   // dev-server: 添加proxy
   devServer: overrideDevServer(addProxy()),
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import styles from './index.module.scss';
 
 export default function Dashboard() {
-  console.log(test);
   return (
     <div className={styles.dashboard}>
       <h2>Dashboard</h2>
