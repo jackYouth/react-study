4b183a0dabb013afba1d840c170a3d72dc0f50b2
suyd <yandong.su@ifchange.com>
2020-08-27 15:00:26 +0800

    feat: commit lint

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -12,13 +12,13 @@ import styles from "./index.module.scss";
 export default function App({ children }) {
   return (
     <div className={styles.app}>
-      <h2>App</h2>
-      {children}
-      <div className={styles.app__footer}>
-        <Link to="/home">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/dashboard">Dashboard</Link>
-      </div>
+             <h2>App</h2>
+            {children}
+            <div className={styles.app__footer}>
+              <Link to="/home">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/dashboard">Dashboard</Link>
+            </div>
     </div>
   );
 }
